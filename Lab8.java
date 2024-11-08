f530b43bec9ff2626f66b37ef898e78269b0b953
FelipeCardozo0 <focardo@emory.edu>
2024-11-08 00:41:45 -1200

    Update Lab8.java

--- Lab8.java
+++ Lab8.java
@@ -1,46 +1,64 @@
-public class Lab8 {
-    public static void main(String[] args) {
-        int[][] elevationMap = {
-                {100, 99, 200, 200, 200, 200, 200, 200, 200, 200},
-                {2000, 98, 2000, 200, 200, 200, 200, 200, 200, 200},
-                {2000, 97, 96, 200, 200, 200, 200, 200, 200, 200},
-                {200, 200, 95, 200, 200, 200, 85, 84, 83, 200},
-                {200, 200, 94, 93, 92, 200, 86, 2000, 82, 200},
-                {200, 150, 200, 2000, 91, 200, 87, 2000, 81, 200},
-                {200, 200, 200, 2000, 90, 89, 88, 2000, 80, 200},
-                {200, 150, 100, 200, 200, 200, 200, 2000, 79, 200},
-                {200, 200, 200, 200, 200, 200, 200, 200, 78, 77},
-                {200, 200, 200, 200, 200, 200, 200, 200, 2000, 76}
-        };
+    public class Lab8 {
+        public static void main(String[] args) {
+            int[][] map = {
+                    {100, 99, 200, 200, 200, 200, 200, 200, 200, 200},
+                    {2000, 98, 2000, 200, 200, 200, 200, 200, 200, 200},
+                    {2000, 97, 96, 200, 200, 200, 200, 200, 200, 200},
+                    {200, 200, 95, 200, 200, 200, 85, 84, 83, 200},
+                    {200, 200, 94, 93, 92, 200, 86, 2000, 82, 200},
+                    {200, 150, 200, 2000, 91, 200, 87, 2000, 81, 200},
+                    {200, 200, 200, 2000, 90, 89, 88, 2000, 80, 200},
+                    {200, 150, 100, 200, 200, 200, 200, 2000, 79, 200},
+                    {200, 200, 200, 200, 200, 200, 200, 200, 78, 77},
+                    {200, 200, 200, 200, 200, 200, 200, 200, 2000, 76}
+            };
 
-        int row = 2;
-        int col = 2;
+            System.out.println(canFlowOffMap(map,0,0));
+            System.out.println(canFlowOffMap(map , 8, 3));
+            System.out.println(canFlowOffMap(map , 1, 3));
+            System.out.println(canFlowOffMap(map , 0, 3));
+            System.out.println(canFlowOffMap(map , 9, 8));
 
-        boolean canFlow = canFlowOffMap(elevationMap, row, col);
-        System.out.println("Can water flow off the map from (" + row + ", " + col + ")? " + canFlow);
+
+        }
+
+        public static boolean canFlowOffMap(int[][] map, int row, int col){
+            int Now_cell= map[row][col];
+
+            if  (row == 0 ||col== 0||row == map.length - 1||col==map[0].length - 1) return true;
+
+    //North
+            if (row > 0 &&Now_cell>map[row-1][col]){
+                return (canFlowOffMap(map, row-1,  col));
+            }
+
+
+
+
+    // South
+                 if (row < map.length - 1 &&Now_cell>map[row+1][col]){
+                return canFlowOffMap(map, row+1,  col);
+            }
+
+
+
+
+    // East
+                        if (col < map[0].length - 1 &&Now_cell>map[row][col+1]){
+                return canFlowOffMap(map, row,  col+1);
+            }
+
+
+
+
+    // west
+                            if (col > 0 &&Now_cell>map[row][col-1]){
+                return canFlowOffMap(map, row,  col-1);
+            }
+
+
+            return false;
+
+        }
+
     }
-
-
-
-
-public static boolean canFlowOffMap(int[][] map, int row, int col) {
-    if (row == 0 || row == map.length - 1 || col == 0 || col == map[0].length - 1) {
-        return true;
-    }
-
-    int currentElevation = map[row][col];
-
-    map[row][col] = Integer.MAX_VALUE;
-
-    if ((row > 0 && map[row - 1][col] < currentElevation && canFlowOffMap(map, row - 1, col)) ||
-            (row < map.length - 1 && map[row + 1][col] < currentElevation && canFlowOffMap(map, row + 1, col)) ||
-            (col > 0 && map[row][col - 1] < currentElevation && canFlowOffMap(map, row, col - 1)) ||
-            (col < map[0].length - 1 && map[row][col + 1] < currentElevation && canFlowOffMap(map, row, col + 1))) {
-        return true;
-    }
-
-    map[row][col] = currentElevation;
-
-    return false;
-}
-}
