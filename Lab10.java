c39291c1895bc086507576865ef2f998fe4a7b53
Felipe Cardozo <focardo@emory.edu>
2024-11-22 00:18:34 -0500

    Update Lab10.java

--- Lab10.java
+++ Lab10.java
@@ -3,12 +3,12 @@ public class Lab10 {
         System.out.println(clearEmojis("LOL :-D"));
         System.out.println(clearEmojis("Smile   :-)"));
         System.out.println(clearEmojis("blah :P teehee :-)"));
+        System.out.println();
         System.out.println(isValidEmail("user_123@gmail.com"));
         System.out.println(isValidEmail("user123alpha@gmail.com"));
         System.out.println(isValidEmail("user@domain.xyz"));
         System.out.println(isValidEmail("123user@domain.com"));
         System.out.println(isValidEmail("user@domaintoolong.com"));
-
     }
     public static boolean isValidEmail(String email) {
         String regex = "^[a-zA-Z][\\w]{0,9}@[a-zA-Z][a-zA-Z0-9]{0,11}\\.[a-z]{1,3}$";
@@ -17,13 +17,10 @@ public class Lab10 {
     }
 
     public static String clearEmojis(String sms) {
-        String regex = "(?<=\\s)(:-\\)|:-D|:-P|:\\)|:D|:P)";
+        String regex = "\\s?[:;][-o*]?[)DPp]";
 
-        String result = sms.replaceAll(regex, ".");
-
-        return result.replaceAll("^\\s+", "");
-    
-        }
+        return sms.replaceAll(regex, ".");
+    }
 
 
 
