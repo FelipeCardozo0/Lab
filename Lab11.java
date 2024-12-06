0a20d05c6f33269ed4b011b13d7df58eff1e49e1
Felipe Cardozo <focardo@emory.edu>
2024-12-05 23:38:22 -1200

    Update Lab11.java

--- Lab11.java
+++ Lab11.java
@@ -1,15 +1,13 @@
 public class Lab11 {
 
     public static void main(String[] args) {
-        // Test case 1: Single word
         System.out.println("Test case 1: Single word");
         countLetters("Hello");
 
-        // Test case 2: Phrase with spaces and punctuation
         System.out.println("\nTest case 2: Phrase with spaces and punctuation");
         countLetters("Hello, World!");
 
-        // Test cases for factorial method
+
         int[] testCases = {5, 10, 17, -5};
 
         for (int n : testCases) {
@@ -31,12 +29,11 @@ public class Lab11 {
             try {
                 counts[word.charAt(i) - 'A']++;
             } catch (ArrayIndexOutOfBoundsException e) {
-                // Print a useful message along with the invalid character
+
                 System.out.println("Not a letter: " + word.charAt(i));
             }
         }
 
-        // Print frequencies
         System.out.println();
         for (int i = 0; i < counts.length; i++) {
             if (counts[i] != 0) {
